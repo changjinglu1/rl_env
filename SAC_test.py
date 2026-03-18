38bb9268f895623b7cd4ef413ad8325281058e08
changjinglu1 <1445710614@qq.com>
2026-03-18 15:40:46 +0800

    测试采用确定性策略

--- SAC_test.py
+++ SAC_test.py
@@ -29,6 +29,18 @@ class SACActor(nn.Module):
         std = log_std.exp()
         return mu, std
 
+    def get_action(self, state, deterministic=True):
+            mu, std = self.forward(state)
+            if deterministic:
+                # 测试模式：直接取均值的 tanh，最稳健，消除随机悬停
+                action = torch.tanh(mu) * self.max_action
+            else:
+                # 训练模式：采样
+                dist = torch.distributions.Normal(mu, std)
+                raw_action = dist.rsample()
+                action = torch.tanh(raw_action) * self.max_action
+            return action
+
     def sample_normal(self, state):
         mu, std = self.forward(state)
         dist = torch.distributions.Normal(mu, std)
@@ -46,11 +58,10 @@ def test_sac(env, actor, episodes=10):
         state, _ = env.reset()
         total_reward = 0
 
-        for _ in range(200):
+        for _ in range(500):
             s = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(device)
 
-            # SAC 必须用 sample_normal
-            action = actor.sample_normal(s)
+            action = actor.get_action(s, deterministic=True)
             action = action.cpu().detach().numpy()[0]
 
             next_state, reward, terminated, truncated, _ = env.step(action)
@@ -68,7 +79,7 @@ def test_sac(env, actor, episodes=10):
 # ===========================
 
 if __name__ == "__main__":
-    env = gym.make("Pendulum-v1")
+    env = gym.make("LunarLanderContinuous-v3", render_mode="human")
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
@@ -76,7 +87,7 @@ if __name__ == "__main__":
     # 修改这里：你的 SAC 模型路径
     current_path = os.path.dirname(os.path.realpath(__file__))
     model = current_path + "/models/"
-    actor_path = model + "sac_actor_20260317182723.pth"
+    actor_path = model + "sac_actor_20260318123131.pth"
 
     # 创建 SAC Actor
     actor = SACActor(state_dim, action_dim, max_action).to(device)
