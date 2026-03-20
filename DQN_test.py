b5b8da8911fc94d3f07a29b143fed1b61fc679e1
changjinglu1 <1445710614@qq.com>
2026-03-20 15:52:48 +0800

    增加roubtness

--- DQN_test.py
+++ DQN_test.py
@@ -6,50 +6,45 @@ from dqn_cartpole import QNet   # 你训练时定义的 QNet
 import time
 import Gridworld
 
-# ============================
-# 1. 加载环境
-# ============================
-env = gym.make("gymnasium_env/GridWorld-v0", render_mode="human")
-env = FlattenObservation(env)
+def dqn_test(env, qnet, episodes):
+    for ep in range(episodes):
+        state, _ = env.reset()
+        env.render()  # 渲染环境
+        total_reward = 0
+        done = False
 
-state_dim = env.observation_space.shape[0]
-action_dim = env.action_space.n
+        while not done:
+            # 选择动作（贪心）
+            with torch.no_grad():
+                action = qnet(torch.tensor(state, dtype=torch.float32)).argmax().item()
 
+            next_state, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
 
-# ============================
-# 2. 加载训练好的模型
-# ============================
-model_path = "models/sac_actor_gridworld20260320144205.pth"   # ← 换成你自己的文件名
+            env.render()  # 渲染环境
 
-qnet = QNet(state_dim, action_dim)
-qnet.load_state_dict(torch.load(model_path))
-qnet.eval()
+            total_reward += reward
+            state = next_state
 
-print("Loaded model:", model_path)
+            time.sleep(0.2)  # 控制渲染速度
 
-# ============================
-# 3. 测试回合
-# ============================
-state, _ = env.reset()
-env.render()  # 渲染环境
-total_reward = 0
-done = False
+        print("Total Reward:", total_reward)
 
-while not done:
-    # 选择动作（贪心）
-    with torch.no_grad():
-        action = qnet(torch.tensor(state, dtype=torch.float32)).argmax().item()
+    env.close()
 
-    next_state, reward, terminated, truncated, info = env.step(action)
-    done = terminated or truncated
+if __name__ == "__main__":
+    # 加载环境
+    env = gym.make("gymnasium_env/GridWorld-v0", render_mode="human")
+    env = FlattenObservation(env)
 
-    env.render()  # 渲染环境
+    state_dim = env.observation_space.shape[0]
+    action_dim = env.action_space.n
 
-    total_reward += reward
-    state = next_state
+    model_path = "models/sac_actor_gridworld20260320144205.pth"   # ← 换成你自己的文件名
 
-    time.sleep(0.2)  # 控制渲染速度
+    qnet = QNet(state_dim, action_dim)
+    qnet.load_state_dict(torch.load(model_path))
+    qnet.eval()
+    print("Loaded model:", model_path)
 
-print("Total Reward:", total_reward)
-
-env.close()
+    dqn_test(env, qnet, 10)
